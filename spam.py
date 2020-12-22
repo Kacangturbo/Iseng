266669165f22a05945d8532426bfb763cc993b3a
Kacangturbo <76491235+Kacangturbo@users.noreply.github.com>
2020-12-23 00:47:35 +0700

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,13 @@
+#spam hayu mabar
+maks = 999
+for i in range(maks):
+  print "gass mabar"
+says = "te"
+jumlah = 20
+while(says == "te"):
+  jumlah += 10
+  says = raw_input("hayu ath euy: ")
+
+maks = 999
+for i in range(maks):
+  print "gass mabar" +str(maks)
